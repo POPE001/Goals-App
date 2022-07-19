8da4759b0192cf5206cc5e3f4df89d0d295520c1
pope001 <adeonioluwamayowa@gmail.com>
2022-07-19 14:57:29 +0100

    Redux Auth completed

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -13,14 +13,31 @@ const initialState = {
 }
 
 
+
 //Register User
-export const register = createAsyncThunk('auth/register', async(user, thunkAPI) => {
+export const register = createAsyncThunk('auth/register', 
+async(user, thunkAPI) => {
     try {
         return await authService.register(user)
     } catch (error) {
         const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
-        return thunkAPI.rejectWithValue()
+        return thunkAPI.rejectWithValue(message)
     }
+})
+
+
+//Login User
+export const login = createAsyncThunk('auth/login', 
+async(user, thunkAPI) => {
+    try {
+        return await authService.login(user)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+export const logout = createAsyncThunk('auth/logout', async() => {
+    await authService.logout()
 })
 
 export const authSlice = createSlice({
@@ -49,6 +66,28 @@ export const authSlice = createSlice({
             state.isError = true
             state.message = action.payload
             state.user = null
+        })
+        
+
+        .addCase(login.pending, (state)=> {
+            state.isLoading = true
+        })
+        .addCase(login.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+            state.user = action.payload
+        })
+        .addCase(login.rejected, (state, action)=> {
+            state.isLoading = false
+            state.isError = true
+            state.message = action.payload
+            state.user = null
+        })
+
+
+        .addCase(logout.fulfilled, (state) => {
+            state.user = null
+
         })
     }
 })
